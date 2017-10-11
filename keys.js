ee73bed29fd688b5259b71f725319aba0f635489
Kathy Satterlee <k.j.satterlee@gmail.com>
2017-10-11 15:18:30 -0400

    "Spotify function created"

--- keys.js
+++ keys.js
@@ -1,8 +1,15 @@
-var twitterKeys = {
-    consumer_key: 'zxSvAX36Eu27mrGU39Wa50318',
-    consumer_secret: 'XNTnOpshCF7xbnLdxJFA60enUeqSAhpMH5qibUZYTuIe50Y9R1',
-    access_token_key: '918137471342465024-pyJeoskss2zv9xzOTFUZ56sOov35OS8',
-    access_token_secret: 'f0WhPDNPrS5nXIm1SdcEIrlJniA8dlwAlgaKQ1DEB1KKF',
-};
+module.exports = {
 
-module.exports = twitterKeys;
+    twitterKeys: {
+        consumer_key: 'zxSvAX36Eu27mrGU39Wa50318',
+        consumer_secret: 'XNTnOpshCF7xbnLdxJFA60enUeqSAhpMH5qibUZYTuIe50Y9R1',
+        access_token_key: '918137471342465024-pyJeoskss2zv9xzOTFUZ56sOov35OS8',
+        access_token_secret: 'f0WhPDNPrS5nXIm1SdcEIrlJniA8dlwAlgaKQ1DEB1KKF'
+    },
+
+    spotifyKeys:  {
+        id: '1279f038b6454101be058829e8022f57',
+        secret: 'c938c64f9aec46d58cdd7a70478b834a'
+    }
+
+};
--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 var input = process.argv;
 var actionCall = input[2];
 var searchTerm = input.slice(3);
+var keys = require('./keys');
 
 switch(actionCall) {
 
@@ -9,7 +10,7 @@ switch(actionCall) {
         break;
 
     case 'spotify-this-song':
-        console.log('Spotify');
+        getSpotifyInfo();
         break;
 
     case 'movie-this':
@@ -27,23 +28,48 @@ switch(actionCall) {
 function showTweets() {
     //Set up twitter client
     var twitter = require('twitter');
-    var twitterKeys = require('./keys');
-    var client = new twitter(twitterKeys);
-
+    var twitterClient = new twitter(keys.twitterKeys);
     var parameters = {
         screen_name: 'kathy_bootcamp',
         count: 20
     };
     //fetch tweets
-    client.get('statuses/user_timeline', parameters, function (error, list, response) {
-        console.log(list);
+    twitterClient.get('statuses/user_timeline', parameters, function (error, list, response) {
 
         if(!error){
             //write tweets
             for (var i = 0; i < list.length; i++) {
                 var currentText = list[i].text;
-                console.log(currentText)
+                console.log(currentText);
+                console.log(' ');
             }
         } else {console.log(error)}
     })
+}
+
+function getSpotifyInfo() {
+    var spotify = require('node-spotify-api');
+    var spotifyClient = new spotify(keys.spotifyKeys);
+
+    if (searchTerm.length === 0) {searchTerm = '"The Sign" Ace of Base'}
+
+    var parameters = {
+        type: 'track',
+        query: searchTerm,
+        limit: 1
+    };
+
+    spotifyClient.search(parameters, function (err, data) {
+        if (!err) {
+            var result = data.tracks.items[0];
+
+            console.log(`Artist: ${result.artists[0].name}`);
+            console.log(`Track: ${result.name}`);
+            console.log(`Album: ${result.album.name}`)
+            console.log(`Preview on Spotify: ${result.preview_url}`)
+
+
+        } else {console.log(err); console.log('error')}
+    })
+
 }
